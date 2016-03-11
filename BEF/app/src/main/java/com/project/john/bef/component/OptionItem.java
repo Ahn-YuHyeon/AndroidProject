d73cd1dae55d08267e1fe69787637c620946b002
Ahn-YuHyeon <unipro87@naver.com>
2016-03-11 13:28:19 +0900

    Add method to handle data.

--- BEF/app/src/main/java/com/project/john/bef/component/OptionItem.java
+++ BEF/app/src/main/java/com/project/john/bef/component/OptionItem.java
@@ -5,4 +5,43 @@ public class OptionItem {
     public int mOprMinute = 0;
     public String mGuideVoice = "";
     public String mRunVoice = "";
+
+    public OptionItem(int oprHour, int oprMinute, String guideVoice, String runVoice) {
+        this.setOprHour(oprHour);
+        this.setOprMinute(oprMinute);
+        this.setGuideVoice(guideVoice);
+        this.setRunVoice(runVoice);
+    }
+
+    public int getOprHour( ) {
+        return mOprHour;
+    }
+
+    public void setOprHour(int oprHour) {
+        mOprHour = oprHour;
+    }
+
+    public int getOprMinute( ) {
+        return mOprMinute;
+    }
+
+    public void setOprMinute(int oprMinute) {
+        mOprMinute = oprMinute;
+    }
+
+    public String getGuideVoice( ) {
+        return mGuideVoice;
+    }
+
+    public void setGuideVoice(String guideVoice) {
+        mGuideVoice = guideVoice;
+    }
+
+    public String getRunVoice( ) {
+        return mRunVoice;
+    }
+
+    public void setRunVoice(String runVoice) {
+        mRunVoice = runVoice;
+    }
 }
