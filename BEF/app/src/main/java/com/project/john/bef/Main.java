47606fdd1a128211ee4836a73bef4721f5c14f53
Ahn-YuHyeon <unipro87@naver.com>
2016-03-13 22:36:16 +0900

    Add button to execute google.

--- BEF/app/src/main/java/com/project/john/bef/Main.java
+++ BEF/app/src/main/java/com/project/john/bef/Main.java
@@ -1,6 +1,10 @@
 package com.project.john.bef;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.speech.RecognizerIntent;
@@ -36,7 +40,8 @@ import java.util.ArrayList;
  * protected int mProtected;
  * }
  */
-public class Main extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class Main extends AppCompatActivity
+        implements TextToSpeech.OnInitListener, SensorEventListener {
     public static DbHelper sDbHelper;
     public static TtsHelper sTtsHelper;
     public static TextToSpeech sTts;
@@ -49,6 +54,21 @@ public class Main extends AppCompatActivity implements TextToSpeech.OnInitListen
 
     AlarmRegister mAlarmRegister;
     AlarmReceiver mAlarmReceiver;
+
+    private long mLastTime;
+    private float mSpeed;
+    private float mLastX;
+    private float mLastY;
+    private float mLastZ;
+    private float mX, mY, mZ;
+
+    private static final int sSHAKE_THRESHOLD = 800;
+        private static final int sDATA_X = SensorManager.DATA_X;
+    private static final int sDATA_Y = SensorManager.DATA_Y;
+    private static final int sDATA_Z = SensorManager.DATA_Z;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerormeterSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +95,8 @@ public class Main extends AppCompatActivity implements TextToSpeech.OnInitListen
                 recognizeVoice( );
             }
         });
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerormeterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     @Override
@@ -126,6 +148,8 @@ public class Main extends AppCompatActivity implements TextToSpeech.OnInitListen
             sTts.shutdown( );
         }
         mAlarmRegister.releaseAlarm(this);
+
+        if (mSensorManager != null) mSensorManager.unregisterListener(this);
         super.finish( );
     }
 
@@ -190,19 +214,66 @@ public class Main extends AppCompatActivity implements TextToSpeech.OnInitListen
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId( );
         switch (id) {
-            case R.id.ic_launcher:
+            case R.id.google:
+                Intent intent = getPackageManager( )
+                        .getLaunchIntentForPackage("com.google.android.googlequicksearchbox");
+                startActivity(intent);
+                break;
+            case R.id.ok_google:
                 if (sFlag) {
                     sTtsHelper.stopTimerTask( );
                     sFlag = false;
                 } else {
-                    Intent intent = getPackageManager( )
-                            .getLaunchIntentForPackage("com.google.android.googlequicksearchbox");
-                    startActivity(intent);
                     sTtsHelper.startSpeak2("오케이구글");
                     sFlag = true;
                 }
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart( ) {
+        super.onStart( );
+        if (mAccelerormeterSensor != null) {
+            mSensorManager
+                    .registerListener(this, mAccelerormeterSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType( ) == Sensor.TYPE_ACCELEROMETER) {
+            long currentTime = System.currentTimeMillis( );
+            long gabOfTime = (currentTime - mLastTime);
+
+            if (gabOfTime > 100) {
+                mLastTime = currentTime;
+                mX = event.values[SensorManager.DATA_X];
+                mY = event.values[SensorManager.DATA_Y];
+                mZ = event.values[SensorManager.DATA_Z];
+
+                mSpeed = Math.abs(mX + mY + mZ - mLastX - mLastY - mLastZ) / gabOfTime * 10000;
+
+                if (mSpeed > sSHAKE_THRESHOLD) {
+                    sTtsHelper.startSpeak("검색어를 말씀하세요!");
+                    SystemClock.sleep(3000);
+                    sTtsHelper.startSpeak("오케이구글");
+                }
+                mLastX = event.values[sDATA_X];
+                mLastY = event.values[sDATA_Y];
+                mLastZ = event.values[sDATA_Z];
+            }
+        }
+    }
+
+    @Override
+    public void onStop( ) {
+        super.onStop( );
+        //if (mSensorManager != null) mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
